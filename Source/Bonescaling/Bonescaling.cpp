b9d240eceec8f9bde6607b0aca6c02155c02a297
Sakyapanchen <sakyapanchen@gmail.com>
2020-06-18 22:09:35 +0300

    Add TP template

---
+++ Source/Bonescaling/Bonescaling.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Bonescaling.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Bonescaling, "Bonescaling" );
